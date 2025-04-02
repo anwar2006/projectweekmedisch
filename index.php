60b7b05247eb0b4c2cd5774c31b91e82409f2452
Timo-alt <6047395@mborijnland.nl>
2025-04-02 11:22:44 +0200

    tailwind toegevoegd

--- index.php
+++ index.php
@@ -4,10 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://code.jquery.com/pep/0.4.3/pep.js" integrity="sha256-yZpZULjaPllFSRFfS6JsDvucyRd3yNo7yKc/YsMQAsk=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <title>Document</title>
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-    
+    <div class="h-screen w-full bg-yellow-100">
+        <header class="w-full h-20 bg-gray-300"></header>
+    </div>
 </body>
 </html>
 
