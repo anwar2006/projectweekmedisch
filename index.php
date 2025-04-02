44bc56bad19528328c69185f754b44c9e050cdad
Alem <106407795+Alem569@users.noreply.github.com>
2025-04-02 09:59:36 +0200

    feat: add jquery cdn

--- index.php
+++ index.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/pep/0.4.3/pep.js" integrity="sha256-yZpZULjaPllFSRFfS6JsDvucyRd3yNo7yKc/YsMQAsk=" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
     
 </body>
 </html>
+
